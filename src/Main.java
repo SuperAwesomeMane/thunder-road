4b169d0679a8dae3987270246b980d5528d87b9e
ashtinh <ashtinhle@gmail.com>
2025-02-16 18:23:17 -0500

    adding more functions

--- src/Main.java
+++ src/Main.java
@@ -7,26 +7,61 @@ public class Main {
 
 		String cars[] = new String[]{"top car", "bottom car"};
 		String directions[] = new String[] {"forward", "back", "up-left", "down-left", "up-right", "down-right"};
+		String playerColor;
+		String colors[] = new String[]{"green", "white", "blue", "purple", "yellow", "orange"};
 
 		Scanner scan = new Scanner(System.in);
-		String input;
+		String input = "";
 
 		System.out.print("Do you want to play? [Yes or No] - ");
 		input = scan.nextLine();
 
-		if (!input.equals("yes") || !input.equals("y")) {	
+		if (!(input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes"))) {	
 			System.out.println("Yes you do!");
 		}
+
+		System.out.println("Select your color.");
+
+		for (int i = 0; i < colors.length; i++) {
+			if (i < colors.length - 1) {
+				System.out.print(colors[i] + ", ");
+			} else {
+				System.out.print(colors[i]);
+			}
+		}
+
 		
-		slam(cars, directions, scan);
+		boolean choosing = true;
+
+		while (choosing) {
+
+			input = scan.nextLine();
+
+			for (int i = 0; i < colors.length; i++) {
+
+				if (input.equalsIgnoreCase(colors[i])) {
+					playerColor = input;
+					System.out.println("you are " + playerColor);
+					choosing = !choosing;
+					break;
+				} else {
+					if (i == colors.length - 1) {
+						System.out.println("Not a valid color");
+						System.out.println("Choose another option: ");
+					}
+				}
+			}
+		}
+
+		//		slam(cars, directions, scan);
 	}
 
 	private static void slam(String[] cars, String[] directions, Scanner scan) {	
-		
+
 		Random random = new Random();
 		boolean playing = true;
 		String input;
-		
+
 		while (playing) {
 			System.out.print("Ready to slam? [Yes or No] - ");
 			input = scan.nextLine();
